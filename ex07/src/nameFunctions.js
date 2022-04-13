2ec20d7d1e3297d1dbeaf521547c742c3ff6a02e
Atrus1 <atrus2012@gmail.com>
2022-04-13 11:12:58 +0200

    Adding files

---
+++ ex07/src/nameFunctions.js
@@ -0,0 +1,12 @@
+const firstName = (firstName) => {
+    return firstName.toUpperCase();
+  }
+  
+   const lastName = (lastName) => {
+    return lastName.toLowerCase();
+  }
+
+ console.log(firstName("Text"));
+ console.log(lastName("Text"));
+ exports.firstName = firstName;
+ exports.lastName = lastName;
---
+++ ex08/src/index.js
@@ -0,0 +1,14 @@
+class Person {
+    constructor(name, email, age) {
+    getPerson () {
+        const personInfo = `Name: ${this.name} email: ${this.email} age: ${this.age}`;
+            }   
+                 return personInfo;
+
+  
+    
+
+
+
+    console.log(getPerson(personInfo));
+    module.exports = Person;
